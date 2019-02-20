7b78b075d5299810aff860a5c17ddd9c271b3912
hicka04 <calpissoda8221@gmail.com>
2019-02-20 21:23:25 +0900

    ファイルとディレクトリの整理

---
+++ github/modules/Root/RootRouter.swift
@@ -0,0 +1,24 @@
+//
+//  RootRouter.swift
+//  github
+//
+//  Created by hicka04 on 2019/02/20.
+//  Copyright © 2019 hicka04. All rights reserved.
+//
+
+import UIKit
+
+final class RootRouter {
+    
+    private let window: UIWindow
+    
+    init(window: UIWindow) {
+        self.window = window
+    }
+    
+    func showFirstView() {
+        let searchResultView = UINavigationController(rootViewController: SearchResultViewController())
+        window.rootViewController = searchResultView
+        window.makeKeyAndVisible()
+    }
+}
---
+++ github/modules/SearchResult/view/SearchResultViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SearchResultViewController.swift
+//  github
+//
+//  Created by hicka04 on 2019/02/20.
+//  Copyright © 2019 hicka04. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+extension SearchResultViewController {
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
