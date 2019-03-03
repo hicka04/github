a57676d8ab4c6f56d8ba978431da8c0620f78620
hicka04 <calpissoda8221@gmail.com>
2019-03-03 12:45:01 +0900

    デフォルトのブランチが存在しないリポジトリはデフォルトのブランチをmasterにする処理追加

--- github/entities/GitHub/Repository.swift
+++ github/entities/GitHub/Repository.swift
@@ -13,11 +13,34 @@ struct Repository: Decodable {
     let id: ID
     let name: String
     let fullName: String
-    let description: String
+    let description: String?
     let stargazersCount: Int
     let language: String?
     let defaultBranch: String
     let owner: User
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case fullName
+        case description
+        case stargazersCount
+        case language
+        case defaultBranch
+        case owner
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        id = try values.decode(ID.self, forKey: .id)
+        name = try values.decode(String.self, forKey: .name)
+        fullName = try values.decode(String.self, forKey: .fullName)
+        description = try values.decodeIfPresent(String.self, forKey: .description)
+        stargazersCount = try values.decode(Int.self, forKey: .stargazersCount)
+        language = try values.decodeIfPresent(String.self, forKey: .language)
+        defaultBranch = try values.decodeIfPresent(String.self, forKey: .defaultBranch) ?? "master"
+        owner = try values.decode(User.self, forKey: .owner)
+    }
 }
 
 extension Repository {
