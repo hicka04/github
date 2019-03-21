01757c995f272124fc29606fc4993b653e22f574
hicka04 <calpissoda8221@gmail.com>
2019-03-21 22:28:48 +0900

    ユーザ検索用Interactor追加

---
+++ github/data/interactor/GithubUser/GithubUserInteractor.swift
@@ -0,0 +1,41 @@
+//
+//  GithubUserInteractor.swift
+//  github
+//
+//  Created by hikaru on 2019/3/21.
+//  Copyright © 2019 hikaru. All rights reserved.
+//
+
+import Foundation
+
+protocol GithubUserUsecase {
+    
+    func searchUsers(from keyword: String,
+                     completion: @escaping (Result<[User], GitHubClientError>) -> Void)
+}
+
+final class GithubUserInteractor {
+    
+    private let client: GitHubRequestable
+    
+    init(client: GitHubRequestable = GitHubClient()) {
+        self.client = client
+    }
+}
+
+extension GithubUserInteractor: GithubUserUsecase {
+    
+    func searchUsers(from keyword: String,
+                     completion: @escaping (Result<[User], GitHubClientError>) -> Void) {
+        let request = GitHubAPI.SearchUsers(keyword: keyword)
+        client.send(request: request) { result in
+            switch result {
+            case .success(let response):
+                completion(.success(response.items))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
+
